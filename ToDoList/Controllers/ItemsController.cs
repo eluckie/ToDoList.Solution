7ff358dc3a3e8c1e80b9036e3715711d3073d984
E Luckie <eluckie.d@gmail.com>
2023-02-28 18:40:33 -0700

    add id's to each new item & apply RESTful routing

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -13,7 +13,7 @@ namespace ToDoList.Controllers
       return View(allItems);
     }
     [HttpGet("/items/new")]
-    public ActionResult CreateForm()
+    public ActionResult New()
     {
       return View();
     }
@@ -22,6 +22,19 @@ namespace ToDoList.Controllers
     {
       Item myItem = new Item(description);
       return RedirectToAction("Index");
+    }
+    [HttpPost("/items/delete")]
+    public ActionResult DeleteAll()
+    {
+      Item.ClearAll();
+      return View();
+    }
+
+    [HttpGet("/items/{id}")]
+    public ActionResult Show(int id)
+    {
+      Item foundItem = Item.Find(id);
+      return View(foundItem);
     }
   }
 }
